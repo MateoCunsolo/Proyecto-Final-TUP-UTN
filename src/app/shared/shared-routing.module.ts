82a95f8d6f1d7697aa2197454e0b0e0354c68cbe
Cunsolo Mateo <cunsolo73@gmail.com>
2023-10-10 20:37:10 -0300

    add footer component

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   {
     path: 'nav',
     component: NavbarComponent
-    }
+    },
+    {
+      path: 'footer',
+      component: FooterComponent
+      },
+
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-  NavbarComponent
+  NavbarComponent,
+  FooterComponent
   ],
   imports: [
     CommonModule,
