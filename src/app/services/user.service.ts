8644679b37c2b13aabf1b60dc1fad8da276abf1d
Cunsolo Mateo <cunsolo73@gmail.com>
2023-10-31 20:47:26 -0300

    add comment done successfully

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,32 +3,26 @@ import { Router } from '@angular/router';
 import { IComment, IUser } from '../core/Interfaces';
 import { Observable, from } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class UserService {
-
   private url = 'http://localhost:3000/users';
   flag: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   getUsers(): Observable<IUser[]> {
-    return from(fetch(this.url).then(response => response.json()));
-
+    return from(fetch(this.url).then((response) => response.json()));
   }
-
 
   public async postUser(user: IUser) {
     try {
-      await fetch(this.url,
-        {
-          method: 'POST',
-          body: JSON.stringify(user),
-          headers: { 'Content-type': 'application/json' }
-        })
+      await fetch(this.url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: { 'Content-type': 'application/json' },
+      });
 
       this.router.navigate(['signin']);
     } catch (error) {
@@ -36,75 +30,57 @@ export class UserService {
     }
   }
 
-
   public async addCommentToUser(userId: number, comment: IComment) {
     try {
-      const user = await fetch(`${this.url}/${userId}`).then(response => response.json());
+      const user = await fetch(`${this.url}/${userId}`).then((response) =>
+        response.json()
+      );
       user.comments.push(comment);
       await fetch(`${this.url}/${userId}`, {
         method: 'PATCH',
         body: JSON.stringify(user),
-        headers: { 'Content-type': 'application/json' }
+        headers: { 'Content-type': 'application/json' },
       });
     } catch (error) {
       console.log(error);
     }
   }
 
-  public async addMovieToList(userId: number, listId: number, movieId: number) {
+  public async addMovieToList(
+    userId: number,
+    listPosChoosen: number,
+    movieId: number
+  ) {
     try {
-
       // Primero, obtén el usuario desde el servidor
-      const user = await fetch(`${this.url}/${userId}`).then(response => response.json());
+      const user = await fetch(`${this.url}/${userId}`).then((response) =>
+        response.json()
+      );
 
       // Agrega el movieId a la lista específica
-      console.log(user.lists[listId].idMovies);
-    
-      for (let i = 0; i <= user.lists[listId].idMovies.length; i++ && this.flag == false) 
-      {
-        if(user.lists[listId].idMovies.length != 0)
-        {
-          if( user.lists[listId].idMovies[i] !== movieId) {
+      console.log(user.lists[listPosChoosen].idMovies);
 
-            user.lists[listId].idMovies.push(movieId);
+      let movieAlreadyInList = false;
+      
+      for (let i = 0; i < user.lists[listPosChoosen].idMovies.length; i++) {
+        if (user.lists[listPosChoosen].idMovies[i] == movieId) {
+          console.log('La pelicula ya se encuentra en la lista'); //la idea es agregar un cartel
+          movieAlreadyInList = true;
+          break;
+        }
+      }
 
-            // Actualiza el usuario en el servidor
-            await fetch(`${this.url}/${userId}`, {
-              method: 'PATCH',
-              body: JSON.stringify(user),
-              headers: { 'Content-type': 'application/json' }
-            });
-          
-            console.log("usuario con peli nueva en la lista:");
-            this.flag = true; 
-
-          }else
-          {
-            console.log("no funca");
-          }
-
-      }else{
-
-        console.log("El arrelgo esta vacio");
-        
-        user.lists[listId].idMovies.push(movieId);
-
-        // Actualiza el usuario en el servidor
+      if (!movieAlreadyInList) {
+        user.lists[listPosChoosen].idMovies.push(movieId);
         await fetch(`${this.url}/${userId}`, {
           method: 'PATCH',
           body: JSON.stringify(user),
-          headers: { 'Content-type': 'application/json' }
+          headers: { 'Content-type': 'application/json' },
         });
-
-      }
       }
 
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error);
     }
   }
-
-
 }
-
