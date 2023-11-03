d372696084fef01fc8141b3b1aca238317d417b1
Mateo Cunsolo <cunsolo73@gmail.com>
2023-11-03 12:24:10 -0300

    update all

---
+++ src/app/shared/components/nav-bar-list/nav-bar-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-list',
+  templateUrl: './nav-bar-list.component.html',
+  styleUrls: ['./nav-bar-list.component.css']
+})
+export class NavBarListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
