4850b9bce944050433cabe5431a20f6c5d2fc1d4
Cunsolo Mateo <cunsolo73@gmail.com>
2023-11-03 20:54:54 -0300

    features: search movie for query

--- src/app/shared/components/input-search/input-search.component.ts
+++ src/app/shared/components/input-search/input-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from 'src/app/services/peliculas.service';
+import { MovieData } from 'src/app/core/movie.interface';
+import { FilteringService } from 'src/app/services/filtering.service';
 
 @Component({
   selector: 'app-input-search',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(public moviesService: PeliculasService, private service : FilteringService) { }
 
   ngOnInit(): void {
+
   }
+  
+  search() {
+    const inputSearch = document.querySelector("#input-search") as HTMLInputElement;  
+    this.service.emitEvent('search', {search: inputSearch.value});
+  }
+
 
 }
