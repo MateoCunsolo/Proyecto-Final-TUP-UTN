a18a178755ade876da60e83ed9093b24a948cd68
Cunsolo Mateo <cunsolo73@gmail.com>
2023-11-04 12:33:34 -0300

    search movies form other components

--- src/app/modules/movies/components/pelicula/pelicula.component.ts
+++ src/app/modules/movies/components/pelicula/pelicula.component.ts
@@ -7,9 +7,8 @@ import { FilteringService } from 'src/app/services/filtering.service';
 @Component({
   selector: 'app-pelicula',
   templateUrl: './pelicula.component.html',
-  styleUrls: ['./pelicula.component.css']
+  styleUrls: ['./pelicula.component.css'],
 })
-
 export class PeliculaComponent implements OnInit {
   public defaultImageURL = 'assets/IMAGE NO DISPONIBLE.png';
   private page = 1;
@@ -17,7 +16,7 @@ export class PeliculaComponent implements OnInit {
 
   movie: Movie = {
     adult: false,
-    backdrop_path: '', 
+    backdrop_path: '',
     genre_ids: [],
     id: 0,
     original_language: '',
@@ -29,7 +28,7 @@ export class PeliculaComponent implements OnInit {
     title: '',
     video: false,
     vote_average: 0,
-    vote_count: 0
+    vote_count: 0,
   };
 
   selectedGenre: number = 0; // Propiedad para almacenar el nombre del género
@@ -39,12 +38,22 @@ export class PeliculaComponent implements OnInit {
   startYear: number = 0;
   valueSearch: string = '';
 
-
-  constructor(private moviesService: PeliculasService, private router: Router, private filterService: FilteringService) {
-  }
+  constructor(
+    private moviesService: PeliculasService,
+    private router: Router,
+    private filterService: FilteringService
+  ) {}
 
   ngOnInit() {
-    this.loadMovies();
+    if (this.router.url === '/home') {
+      this.movies = [];
+      this.loadMovies();
+    } else {
+      this.movies = [];
+      this.valueSearch = this.router.url.split('=')[1];
+      this.loadMoviesFromSearch(this.valueSearch);
+      this.router.navigate(['/home']);
+    }
 
     this.filterService.getEvent('filterGenre').subscribe((event) => {
       this.selectedGenre = event.data.idgenre;
@@ -69,72 +78,88 @@ export class PeliculaComponent implements OnInit {
 
     this.filterService.getEvent('search').subscribe((event) => {
       this.movies = [];
-      if(event.data.search === 'remove')
-      {
+      const searchValue = event.data.search;
+      if (
+        searchValue === 'remove' ||
+        this.conteinWordsAndNonAlphanumeric(searchValue)
+      ) {
         this.loadMovies();
-      }else
-      {
-        this.loadMoviesFromSearch(event.data.search);
-        this.valueSearch = event.data.search;
+      } else {
+        this.loadMoviesFromSearch(searchValue);
+        this.valueSearch = searchValue;
       }
-      
     });
   }
- 
+
+  conteinWordsAndNonAlphanumeric(search: string): boolean {
+    if (!/^[a-zA-Z0-9\s]*$/.test(search)) {
+      return true;
+    }
+
+    return false;
+  }
+
   loadMovies() {
-    this.moviesService.listMovies(this.page)
+    this.moviesService
+      .listMovies(this.page)
       .then((data: MovieData) => {
         this.movies = this.movies.concat(data.results);
-      }) 
-      .catch(error => {
+      })
+      .catch((error) => {
         console.error('Error fetching data:', error);
-      
       });
   }
-  
+
   loadMoviesFromSearch(search: string) {
-    if (this.valueSearch === '') {
+    if (search === '') {
       this.movies = [];
       this.loadMovies();
-    }
-    else
-    {
-    this.moviesService.listMoviesFromSearch(search, this.page)
-      .subscribe((data: MovieData) => {
-        this.movies = this.movies.concat(data.results);
-      }, error => {
-        console.error('Error fetching data:', error);
-      });
-
+    } else {
+      this.moviesService.listMoviesFromSearch(search, this.page).subscribe(
+        (data: MovieData) => {
+          this.verifyData(data);
+        },
+        (error) => {
+          alert('No se encontraron resultados');
+        }
+      );
     }
   }
-  
 
   loadMoviesByGenre() {
     if (this.selectedGenre === 0) {
       this.movies = [];
       this.loadMovies();
     } else {
-      this.moviesService.listMoviesByGenre(this.page, this.selectedGenre)
-        .subscribe((data: MovieData) => { // Cambiamos .then() por .subscribe()
-          this.movies = this.movies.concat(data.results);
-        }, error => {
-          console.error('Error fetching data by genre:', error);
-        });
+      this.moviesService
+        .listMoviesByGenre(this.page, this.selectedGenre)
+        .subscribe(
+          (data: MovieData) => {
+            // Cambiamos .then() por .subscribe()
+            this.verifyData(data);
+          },
+          (error) => {
+            console.error('Error fetching data by genre:', error);
+          }
+        );
     }
   }
-  
+
   loadMoviesByRating() {
     if (this.selectedRating === '') {
       this.movies = [];
       this.loadMovies();
     } else {
-      this.moviesService.listMoviesByRating(this.page, this.selectedRating)
-        .subscribe((data: MovieData) => {
-          this.movies = this.movies.concat(data.results);
-        }, error => {
-          console.error('Error fetching data by rating:', error);
-        });
+      this.moviesService
+        .listMoviesByRating(this.page, this.selectedRating)
+        .subscribe(
+          (data: MovieData) => {
+            this.verifyData(data);
+          },
+          (error) => {
+            console.error('Error fetching data by rating:', error);
+          }
+        );
     }
   }
 
@@ -143,51 +168,48 @@ export class PeliculaComponent implements OnInit {
       this.movies = [];
       this.loadMovies();
     } else {
-      this.moviesService.listMoviesByRangeYear(this.page, this.startYear, this.endYear)
-        .subscribe((data: MovieData) => {
-          this.movies = this.movies.concat(data.results);
-        }, error => {
-          console.error('Error fetching data by range year:', error);
-        });
+      this.moviesService
+        .listMoviesByRangeYear(this.page, this.startYear, this.endYear)
+        .subscribe(
+          (data: MovieData) => {
+            this.verifyData(data);
+          },
+          (error) => {
+            console.error('Error fetching data by range year:', error);
+          }
+        );
     }
-
   }
-  
+
+  verifyData(data: MovieData) {
+    if (data.results.length === 0) {
+      alert('No se encontraron resultados');
+    } else {
+      this.movies = this.movies.concat(data.results);
+    }
+  }
 
   loadNextPage() {
-    if(this.selectedGenre != 0)
-    {
+    if (this.selectedGenre != 0) {
       this.page++;
       this.loadMoviesByGenre();
-
-    }else if(this.selectedRating != '')
-    {
+    } else if (this.selectedRating != '') {
       this.page++;
       this.loadMoviesByRating();
-      
-    }else if(this.selectedYear != 0)
-    {
+    } else if (this.selectedYear != 0) {
       this.page++;
       this.loadMoviesByRangeYear();
-
-    }else if(this.valueSearch != '')
-    {
-        this.page++;
-        this.loadMoviesFromSearch(this.valueSearch);
-    }else
-    {
+    } else if (this.valueSearch != '' || this.router.url != '/home') {
+      this.page++;
+      this.loadMoviesFromSearch(this.valueSearch);
+    } else {
       this.page++;
       this.loadMovies();
     }
-    
   }
 
   redirectToMovieDetail(movieClicked: Movie) {
     sessionStorage.setItem('movieClicked', JSON.stringify(movieClicked));
     this.router.navigate(['home/movie/' + movieClicked.id]);
   }
-
-
-  
 }
-
