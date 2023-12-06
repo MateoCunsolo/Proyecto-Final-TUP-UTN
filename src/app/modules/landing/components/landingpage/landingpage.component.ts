a923c18579ec7a83f2f9d521c9eae0e06ab691b7
magali <maguimourino@gmail.com>
2023-12-06 09:28:50 -0300

    changes landing page

--- src/app/modules/landing/components/landingpage/landingpage.component.ts
+++ src/app/modules/landing/components/landingpage/landingpage.component.ts
@@ -10,11 +10,32 @@ export class LandingpageComponent {
   
   constructor(private router: Router) {}
 
+  images = [
+    './assets/movie-1.png',
+    './assets/movie-2.png',
+    './assets/movie-3.png',
+    './assets/movie-4.png'
+    // Añade más imágenes según sea necesario
+  ];
+
+  carouselOptions = {
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000, // ajusta el tiempo de reproducción automática
+  };
+
   onSignUpClick() {
     this.router.navigate(['/signup']);
   }
-  
+
   onSignInClick() {
     this.router.navigate(['/signin']);
   }
+
+
+    
+  
+    
+  
 }
